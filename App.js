ff1e4626d27e03e5caf772a86ed273e3087eaec0
raghuhr18 <raghuhr18@yahoo.com>
2023-07-17 23:03:42 +0530

    Running the CountDown Timer

---
+++ App.js
@@ -0,0 +1,31 @@
+
+import React, {useState} from 'react'
+import { StyleSheet, Text, View, Platform } from 'react-native';
+import { Focus } from './src/features/focus/Focus';
+import { Timer } from './src/features/timer/Timer';
+import { colors } from './src/utils/colors';
+import { spacing } from './src/utils/sizes';
+
+
+
+export default function App() {
+  const [focusSubject, setFocusSubject] = useState("Gardening")
+  return (
+    <View style={styles.container}>
+      {focusSubject ? (
+        <Timer focusSubject={focusSubject}/>
+      ) : (
+        <Focus addSubject={setFocusSubject}/>
+        )}
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop:  Platform.OS === 'ios'? spacing.md: spacing.lg,
+    backgroundColor: colors.darkBlue,
+  },
+});
